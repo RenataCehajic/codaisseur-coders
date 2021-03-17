3e430cc98cced1f489a5e7677e7735b8b4b511d8
RenataCehajic <renata.cehajic@gmail.com>
2021-03-17 16:02:37 +0100

    Fetching data with thunk

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import { fetchPost } from "../store/postPage/actions";
 
 export default function PostPage() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchPost(id));
+  }, [dispatch, id]);
+
   return (
     <div>
       <h1>Hello!</h1>
